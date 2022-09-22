27ce750678ff48cadcadbcdd5f5e889010e756e5
nutsaabazadze <39191752+nutsaabazadze@users.noreply.github.com>
2022-09-22 12:23:38 +0400

    Add files via upload

---
+++ Northwind_SQL1_withoutcode.sql
@@ -0,0 +1,55 @@
+/* 1. Get Order id, Product id, Unit price from Order Details. */
+
+/* 2. Find Title of employee Nancy. */
+
+/* 3. Get the price of an order (by multiplying unit 
+price by quantity) from Order Details.*/
+
+/* 4. Display all cities that employees belong to 
+but don’t allow repetition.*/
+
+/* 5. Find complete name of all employees.*/
+
+/* 6. Display data of all employees those working as 
+     Sales Representative.*/
+
+/* 7. Display complete name of employees those 
+      lives in London.*/
+
+/* 8. Display product name whose unit price 
+      are greater than 90$.*/
+
+/* 9. List the name of all employees whose first 
+      name starts with the letter ‘A’.*/
+
+/* 10. In Customer table, display all 
+       cities that ends with the letter ‘a’.*/
+
+/* 11. Display names of all employees whose 
+       name contain ‘an’.*/
+
+/* 12. Display all the orders where unit price 
+       lies in the range of 10$ to 40$.*/
+
+/* 13. Display the company name where Region 
+       is NULL in Customer Table.*/
+
+/* 14. Write a query to list employees who live 
+       in London, Seattle or Redmond*/
+
+/* 15. Write a query to list employees whose address 
+     does not contain Rd.*/
+
+/* 16. Write a query to list all those employees 
+   whose TitleofCourtesy does not starts with M.*/
+
+
+/* 17. List order details whose ShipRegion is not Null.*/
+
+
+/* 18. List all products where UnitPrice is 
+  between 10 and 15 and QuantityPerUnit contains “bottles”*/
+
+/* 19. List all products where UnitPrice is not  
+      in 10,12,15,17 or 19.*/
+
