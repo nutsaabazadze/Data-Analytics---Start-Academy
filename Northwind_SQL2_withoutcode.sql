458e4af731e3d52cc2461240203d435214da4659
nutsaabazadze <39191752+nutsaabazadze@users.noreply.github.com>
2022-09-22 12:31:33 +0400

    Delete Northwind_SQL2_withoutcode.sql

--- Northwind_SQL2_withoutcode.sql
+++
@@ -1,39 +0,0 @@
-/*1. Create a report that shows the capitalized FirstName and capitalized LastName renamed as 
-     FirstName and Lastname respectively and HireDate from the employees table sorted from the 
-     newest to the oldest employe*/
-
-
-/*2. Create a report that shows the top 10 OrderID, OrderDate, ShippedDate, CustomerID, 
-     Freight from the orders table sorted by Freight in descending order */
-
-
-/*3. Create a report showing all the ContactName, Address,      City of all customers not from Germany, Mexico, Spain.*//*4. Create a report that shows the EmployeeID, OrderID, 
-     CustomerID, RequiredDate, 
-     ShippedDate from all orders shipped later
-     than the required date */
-
-
-/*5. Create a report that shows the EmployeeID, OrderID, CustomerID, RequiredDate, 
-     ShippedDate from all orders shipped later
-     than the required date and calculate how any days later it shipped and sort by delayed days*/
-
-
-/*6. Create a report showing all the even numbers of OrderID from the orders table */
-
-/*7. Create a report that shows the FirstName, LastName 
-     of all employees that do not report to anybody */
-
-
-/*8. Create a report that shows the SupplierID, ProductName, 
-     CompanyName from all product Supplied by Exotic Liquids,
-     Specialty Biscuits, Ltd., Escargots Nouveaux sorted by the supplier ID */
-
-
-/*9. Create a select statement that outputs the following 
-      "FirstName LastName can be reached atx Extension"*/
-
-/*10. Create a report that shows the SupplierID, CompanyName, CategoryName, 
-      ProductName and UnitPrice from the products, 
-      suppliers and categories table. */ /* 11. Create a report that shows the CustomerID, sum of Freight, 
- from the orders table with sum of freight greater $200, grouped 
- by CustomerID.  */
